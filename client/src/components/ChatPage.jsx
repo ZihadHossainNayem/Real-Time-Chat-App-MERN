579befedaae391c636b6e2b589a55fc5cf838ac0
Zihad Hossain Nayem <zihadhossainnayem@gmail.com>
2023-10-16 15:17:17 +0600

    sending new message through form submit

--- client/src/components/ChatPage.jsx
+++ client/src/components/ChatPage.jsx
@@ -8,6 +8,8 @@ export const ChatPage = () => {
   const [onlineClient, setOnlineClient] = useState([]);
   /* state for chat selection */
   const [selectedUser, setSelectedUser] = useState(null);
+  /* state for new message */
+  const [newMessage, setNewMessage] = useState("");
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:5559");
@@ -40,6 +42,19 @@ export const ChatPage = () => {
   const { id } = useContext(UserContext);
   const onlineClientExcludeOwnUsername = { ...onlineClient };
   delete onlineClientExcludeOwnUsername[id];
+
+  /* function to send new message */
+  const sendMessage = (e) => {
+    e.preventDefault();
+    ws.send(
+      JSON.stringify({
+        message: {
+          recipient: selectedUser,
+          text: newMessage,
+        },
+      })
+    );
+  };
 
   return (
     <div className=" h-screen grid grid-cols-10">
@@ -75,7 +90,7 @@ export const ChatPage = () => {
       </div>
       {/* right grid */}
       <div className="col-span-7 bg-purple-100  px-3 md:px-6 pt-3 md:pt-8 flex flex-col">
-        <h1 className="md:text-2xl text-lg md:font-bold font-bold flex-grow">
+        <div className="md:text-2xl text-lg md:font-bold font-bold flex-grow">
           {!selectedUser && (
             <div className="flex items-center justify-center h-full">
               <p className="text-gray-500">
@@ -83,25 +98,32 @@ export const ChatPage = () => {
               </p>
             </div>
           )}
-        </h1>
-        <div className="flex gap-2 mb-2">
-          <input
-            type="text"
-            placeholder="type a message..."
-            className="border border-purple-200 p-2 flex-grow"
-          />
-          <button className="bg-purple-200 py-2 px-3 border border-purple-200">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />{" "}
-            </svg>
-          </button>
         </div>
+        {!!selectedUser && (
+          <form className="flex gap-2 mb-2" onSubmit={sendMessage}>
+            <input
+              type="text"
+              placeholder="type a message..."
+              className="border border-purple-200 p-2 flex-grow focus:outline-purple-300 "
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="bg-purple-200 py-2 px-3 border border-purple-200"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />{" "}
+              </svg>
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
